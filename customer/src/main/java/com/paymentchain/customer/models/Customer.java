937c9b2b78b6832cbfa60f28ed6d482ac7f3a556
Ivan Frias <friasgilivan@gmail.com>
2024-01-26 12:42:16 +0100

    Funciona con lombok

--- customer/src/main/java/com/paymentchain/customer/models/Customer.java
+++ customer/src/main/java/com/paymentchain/customer/models/Customer.java
@@ -5,6 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Customer {
     @Id
@@ -12,31 +21,4 @@ public class Customer {
     private Long id;
     private String name;
     private String phone;
-    
-    public Customer() {
-    }
-
-    public Customer(Long id, String name, String phone) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
--- customer/src/main/java/com/paymentchain/customer/models/dto/CustomerDTO.java
+++ customer/src/main/java/com/paymentchain/customer/models/dto/CustomerDTO.java
@@ -1,28 +1,16 @@
 package com.paymentchain.customer.models.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class CustomerDTO {
     private String name;
     private String phone;
-
-    public CustomerDTO(){
-
-    }
-
-    public CustomerDTO(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
